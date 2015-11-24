d5df7ad390926f32e37b9c5091854dd57934c796
Kait Smith <ksmith@zeel.com>
2015-11-24 15:07:03 -0500

    Nov. 24 2015 Commits

---
+++ my-class-work/class-07-intro-to-programming/first_javascript/script.js
@@ -0,0 +1,20 @@
+alert('Meow.');
+
+document.write('writing the document.')
+
+document.write(10*10);
+
+document.write(100/10);
+
+document.write(50-17);
+
+document.write(50+17);
+
+document.write(' kait ' + 'smith ');
+
+var num1 = 10 * 10;
+
+document.write(num1);
+
+document.getElementById('title').style.color='red';
+
